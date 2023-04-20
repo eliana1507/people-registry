6b871795aa18bbc143b26bb825844da391b6a964
elianacappello <eliana.cappello87@gmail.com>
2023-04-20 08:09:33 +0200

    Save children recursively and manage person not found use case

--- src/test/java/com/example/peopleregistry/service/PeopleServiceTest.java
+++ src/test/java/com/example/peopleregistry/service/PeopleServiceTest.java
@@ -6,8 +6,12 @@ import org.junit.jupiter.api.Test;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 
 class PeopleServiceTest {
     PeopleService service;
@@ -33,6 +37,35 @@ class PeopleServiceTest {
     }
 
     @Test
+    void saveExistingPerson() {
+        Person p1 = new Person("196712137165", "Pippo Bianchi",
+                "Teresa Bianchi", 65, new ArrayList<>());
+
+        service.savePerson(p1);
+        p1.setAge(55);
+        service.savePerson(p1);
+        assertEquals(p1.getAge(), service.personMap.get(p1.getPnr()).getAge());
+    }
+
+    @Test
+    void savePersonWithChildrenAndGrandChildren() {
+        List<Person> granChildren = new ArrayList<>();
+        granChildren.add(new Person("202207152837", "Martina Bianchi", "", 1, new ArrayList<>()));
+        granChildren.add(new Person("202004172938", "Domenico Bianchi", "", 3, new ArrayList<>()));
+        List<Person> children = new ArrayList<>();
+        children.add(new Person("198707152837", "Alessio Bianchi", "", 35, granChildren));
+        children.add(new Person("199004172938", "Carlo Bianchi", "", 33, new ArrayList<>()));
+
+        Person p1 = new Person("195710137865", "Pippo Bianchi",
+                "Teresa Bianchi", 65, children);
+
+        Person saved = service.savePerson(p1);
+
+        assertTrue(service.personMap.containsKey("202207152837"));
+        assertNotNull(service.getPersonByPnr("202004172938"));
+    }
+
+    @Test
     void getPersonByPnr() {
         Person marioRossi = new Person("201408093645", "Mario Rossi", "", 9, new ArrayList<>());
         Person ilariaRossi = new Person("201512063645", "Ilaria Rossi", "", 8, new ArrayList<>());
@@ -46,13 +79,20 @@ class PeopleServiceTest {
 
         service.personMap.put("201408093645", marioRossi);
         service.personMap.put("201512063645", ilariaRossi);
-        Person get = service.getPersonByPnr("199007017865");
+        Optional<Person> get = service.getPersonByPnr("199007017865");
 
-        assertNotNull(get);
-        assertEquals("Piero Rossi", get.getName());
+        assertTrue(get.isPresent());
+        assertEquals("Piero Rossi", get.get().getName());
 
-        Person child = service.getPersonByPnr("201512063645");
-        assertEquals("Ilaria Rossi", child.getName());
+        Optional<Person> child = service.getPersonByPnr("201512063645");
+        assertTrue(child.isPresent());
+        assertEquals("Ilaria Rossi", child.get().getName());
+    }
+
+    @Test
+    void personNotFound() {
+        Optional<Person> notAPerson = service.getPersonByPnr("notAPnr");
+        assertTrue(notAPerson.isEmpty());
     }
 
     @Test
